70b374aad00714a8ae21502ffc5d2f65df1c9f3d
sachalmalick <sachalmalick@gmail.com>
2016-10-05 13:56:07 -0400

    updated code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import random
+import random, csv
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -15,12 +15,94 @@ def pagetwo():
 
     return render_template("login.html")
 
-@app.route("/auth", methods=['POST'])
-def authenitcate():
-    if (request.form["username"] == "patrick" and request.form["password"] == "star" ):
-        return render_template("success.html")
+def readcsv():
+    with open('data.csv', 'rb') as csvfile:
+        read = csv.reader(csvfile)
+        listify = list(read)
+        return listify
+
+
+def writecsv(username,password):
+    with open('data.csv','a') as csvfile:
+        write = csv.writer(csvfile)
+        write.writerow([username,password])
+
+def checkexist(username):
+    checklist = readcsv()
+    for x in checklist:
+        if(x[0] == username):
+            return True
+    return False
+
+def login(username,password):
+    if(checkexist(username)):
+        for x in filelist:
+            if(x[0] == username):
+                if(x[1] == password):
+                    return render_template("success.html")
+                else:
+                    return render_template("login.html",displaymessage = "Incorrect password")
+                
     else:
-        return render_template("failed.html")
+        return render_template("login.html",displaymessage = "That username doesnt exist")
+    
+@app.route("/sort", methods=['POST'])
+def sort():
+    username = request.form["username"]
+    password = request.form["password"]
+    filelist = readcsv()
+    if (request.form["go"] == "login"):
+        if(checkexist(username)):
+            for x in filelist:
+                if(x[0] == username):
+                    if(x[1] == password):
+                        return render_template("success.html")
+                    else:
+                        return render_template("login.html",displaymessage = "Incorrect password")
+                
+        else:
+            return render_template("login.html",displaymessage = "That username doesnt exist")
+    else:
+        if(checkexist(username)):
+            return render_template("login.html",displaymessage = "That username already exists")
+        else:
+            writecsv(username,password)
+            
+            return render_template("login.html",displaymessage = "Account successfully created")
+        
+    
+        
+            
+    
+
+@app.route("/auth", methods=['POST'])
+def authenitcate(username,password):
+    #username = request.form["username"]
+    #password = request.form["password"]
+    if(checkexist(username)):
+        for x in filelist:
+            if(x[0] == username):
+                if(x[1] == password):
+                    return render_template("success.html")
+                else:
+                    return render_template("login.html",displaymessage = "Incorrect password")
+
+    else:
+        return render_template("login.html",displaymessage = "That username doesnt exist")
+
+    
+@app.route("/register", methods=['POST'])
+def register(username,password):
+    #username = request.form["username"]
+    #password = request.form["password"]
+    if(checkexist(username)):
+        return render_template("login.html",displaymessage = "That username already exists")
+    else:
+        writecsv(username,password)
+        return render_template("login.html",displaymessage = "Account successfully created")
+        
+    
+
     
 
     
